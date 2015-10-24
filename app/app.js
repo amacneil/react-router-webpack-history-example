168ee1128548e7a499ce227b558911d6ab4245d1
Adrian Macneil <adrian@adrianmacneil.com>
2015-10-23 19:10:51 -0700

    Add nested route

--- app/app.js
+++ app/app.js
@@ -13,8 +13,19 @@ var Home = React.createClass({
 var About = React.createClass({
   render() {
     return (
-      <h2>About</h2>
+      <div>
+        <h2>About</h2>
+        {this.props.children}
+      </div>
     );
+  }
+});
+
+var Nested = React.createClass({
+  render() {
+    return (
+      <p>Nested content.</p>
+    )
   }
 });
 
@@ -25,6 +36,7 @@ var App = React.createClass({
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
+          <li><Link to="/about/nested">About &raquo; Nested</Link></li>
         </ul>
         {this.props.children}
       </div>
@@ -36,7 +48,9 @@ React.render(
   <Router history={createBrowserHistory()}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
-      <Route path="about" component={About} />
+      <Route path="about" component={About}>
+        <Route path="nested" component={Nested} />
+      </Route>
     </Route>
   </Router>,
   document.getElementById("app")
