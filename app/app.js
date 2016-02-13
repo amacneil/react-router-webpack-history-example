03159f9fe8549afb08d24a6559364834aba978b9
Ignacio Ureta <ignacio.ureta@live.com>
2016-02-13 18:59:12 -0300

    redux working, phew! FINALLY

--- app/app.js
+++ app/app.js
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom";
 import _ from "underscore";
+
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import { addTodo, completeTodo, setVisibilityFilter, VisibilityFilters } from './redux/actions/actions.js'
+import todoApp from './redux/reducers/reducers.js'
+
+
 import { Router, Route, IndexRoute, Link, History } from "react-router"
 import createBrowserHistory from 'history/lib/createBrowserHistory'
 
-require('./css/style.scss');
+require('./css/style.scss')
+
+
+let store = createStore(todoApp)
 
 var Home = React.createClass({
   render() {
@@ -37,9 +45,25 @@ var Nested = React.createClass({
 
 var App = React.createClass({
   componentDidMount: function(){
-    _.each([1,2,3], function(num){ 
-      console.log(num); 
-    });
+    // Log the initial state
+    console.log(store.getState())
+
+    // Every time the state changes, log it
+    // Note that subscribe() returns a function for unregistering the listener
+    let unsubscribe = store.subscribe(() =>
+      console.log(store.getState())
+    )
+
+    // Dispatch some actions
+    store.dispatch(addTodo('Learn about actions'))
+    store.dispatch(addTodo('Learn about reducers'))
+    store.dispatch(addTodo('Learn about store'))
+    store.dispatch(completeTodo(0))
+    store.dispatch(completeTodo(1))
+    store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+
+    // Stop listening to state updates
+    unsubscribe();
   },
   render: function(){
     var self = this;
@@ -57,13 +81,15 @@ var App = React.createClass({
 });
 
 ReactDom.render(
-  <Router history={createBrowserHistory()}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home} />
-      <Route path="about" component={About}>
-        <Route path="nested" component={Nested} />
+  <Provider store={store}>
+    <Router history={createBrowserHistory()}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="about" component={About}>
+          <Route path="nested" component={Nested} />
+        </Route>
       </Route>
-    </Route>
-  </Router>,
+    </Router>
+  </Provider>,
   document.getElementById("app")
 );
